6195172d32e992a683ade8489330b1923c8e45ba
Jacob Lane <jacobdlane2002@gmail.com>
2020-07-14 16:08:25 -0400

    jQuery was running before DOM loaded, added listener to wait for load

--- public/js/chatroom.js
+++ public/js/chatroom.js
@@ -1,12 +1,12 @@
 var outputArea = $("#chat-output");
 
-$("#user-input-form").on("submit", function(e) {
-  
-  e.preventDefault();
-  
-  var message = $("#user-input").val();
-  
-  outputArea.append(`
+$("document").ready(() => {
+  $("#user-input-form").on("submit", function (e) {
+    e.preventDefault();
+
+    var message = $("#user-input").val();
+
+    outputArea.append(`
     <div class='bot-message'>
       <div class='message'>
         hiii as well
@@ -14,17 +14,17 @@ $("#user-input-form").on("submit", function(e) {
       </div>
     </div>
   `);
-  
-  setTimeout(function() {
-    outputArea.append(`
+
+    setTimeout(function () {
+      outputArea.append(`
       <div class='user-message'>
         <div class='message'>
           Heyloooowww
         </div>
       </div>
     `);
-  }, 250);
-  
-  $("#user-input").val("");
-  
+    }, 250);
+
+    $("#user-input").val("");
+  });
 });
