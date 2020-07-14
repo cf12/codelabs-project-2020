4eeef908101ec7178cadf558e8d5a8bf7e681858
Arianne Ghislaine Rull <arianneghislaine@gmail.com>
2020-07-14 15:58:05 -0400

    test files

---
+++ views/test/chatroom.js
@@ -0,0 +1,30 @@
+var outputArea = $("#chat-output");
+
+$("#user-input-form").on("submit", function(e) {
+  
+  e.preventDefault();
+  
+  var message = $("#user-input").val();
+  
+  outputArea.append(`
+    <div class='bot-message'>
+      <div class='message'>
+        hiii as well
+        ${message}
+      </div>
+    </div>
+  `);
+  
+  setTimeout(function() {
+    outputArea.append(`
+      <div class='user-message'>
+        <div class='message'>
+          Heyloooowww
+        </div>
+      </div>
+    `);
+  }, 250);
+  
+  $("#user-input").val("");
+  
+});
