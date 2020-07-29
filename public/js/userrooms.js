71e95935aaa1046a791b76a24b75c7e08c8a5201
Jacob Lane <jacobdlane2002@gmail.com>
2020-07-29 11:49:17 -0400

    style and text changes and userrooms delete works again

--- public/js/userrooms.js
+++ public/js/userrooms.js
@@ -2,6 +2,7 @@ $("document").ready(() => {
   //event listener for delete room
   $(".delete-button").each((index, value) => {
     $(value).on("click", () => {
+      console.log("dsffa");
       $.get("/deleteroom", { rid: value.id }, (success) => {
         if (success) {
           window.location.href = window.location.href;
